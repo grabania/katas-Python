894982e58a71764062b0691335ae1f0bee2169db
grabania <grabania.work@gmail.com>
2020-03-30 22:17:08 +0100

    Create sum of positive

--- katas_8kyu.py
+++ katas_8kyu.py
@@ -112,3 +112,24 @@ def opposite(number):
 
 
 print(opposite(1))
+
+
+
+# Sum of positive
+# You get an array of numbers, return the sum of all of the positives ones.
+# Example[1, -4, 7, 12] = > 1 + 7 + 12 = 20
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    s = 0
+    for x in arr:
+       if x > 0:
+           s = s + x
+    return s
+
+
+print(positive_sum([1, 2, 3, 4, 5]))
+print(positive_sum([1, -2, 3, 4, 5]))
+print(positive_sum([-1, 2, 3, 4, -5]))
+print(positive_sum([]))
+print(positive_sum([-1, -2, -3, -4, -5]))
