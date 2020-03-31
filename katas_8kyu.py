90fdecfbb10ce79d4d6685468e6765c6c6f9b547
grabania <grabania.work@gmail.com>
2020-03-31 23:24:44 +0100

    Create convert boolean value

--- katas_8kyu.py
+++ katas_8kyu.py
@@ -154,3 +154,21 @@ def make_negative(number):
         return number
     else:
         return number * (-1)
+
+
+print(make_negative(42))
+
+
+# Convert boolean values to strings 'Yes' or 'No'.
+# Complete the method that takes a boolean value and 
+# return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    if boolean == True:
+        return 'Yes'
+    else:
+        return 'No'
+
+
+print(bool_to_word(True))
+print(bool_to_word(False))
