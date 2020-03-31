a2f4220854347d43a8ddfa4dda35d83fb0cbfae7
grabania <grabania.work@gmail.com>
2020-03-31 22:51:44 +0100

    Create negative

--- katas_8kyu.py
+++ katas_8kyu.py
@@ -133,3 +133,24 @@ print(positive_sum([1, -2, 3, 4, 5]))
 print(positive_sum([-1, 2, 3, 4, -5]))
 print(positive_sum([]))
 print(positive_sum([-1, -2, -3, -4, -5]))
+
+
+
+# Return Negative
+# In this simple assignment you are given a number 
+# and have to make it negative. But maybe the number is already negative?
+# Example:
+# make_negative(1)  # return -1
+# make_negative(-5)  # return -5
+# make_negative(0)  # return 0
+# Notes:
+# The number can be negative already, in which case 
+# no change is required.
+# Zero(0) is not checked for any specific sign. 
+# Negative zeros make no mathematical sense.
+
+def make_negative(number):
+    if number <= 0:
+        return number
+    else:
+        return number * (-1)
